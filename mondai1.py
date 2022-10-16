19a182d196454321311139a3025404edec3a5062
jose1428 <nguyenvanthi168@gmail.com>
2022-10-17 02:28:10 +0900

    modified

--- mondai1.py
+++ mondai1.py
@@ -1,38 +1,38 @@
-# ３条件付きで入力
-# ミスしたらまたは何も入力されてない場合再入力させる
+def nhaplieu(a):
+    b=''
+    for i in a:
+        if i!=' ':
+            b=b+i
+
+        #cắt chuỗi b thành list
+    global mylist
+    mylist = b.split(',')
+
+        #xoá phần tử rỗng trong list
+    for i in mylist:
+        if i=='':
+            mylist.remove(i)
+
+    co=1
+    for i in mylist:
+        if kiemtra(i)==0:
+            co=0
+    return co
+            
 
 
+def kiemtra(x):
+    if x.isdigit():
+        return 1
+    else:
+        return 0
 
 
-# a =input('文字列を入力\n')
-# if(a ==''):
-#     a =input('再入力\n')
-
-# for i in range(len(a)):
-#    if (a[i].isnumeric()==False):
-#        if (a[i]!=','):
-#            if(a[i]!=' '):
-#                a =input('再入力\n') 
-
-             
-# mylist = a.split(',') 
-# for x in mylist:
-#     print(x)              
 a=input('nhập dữ liệu vào ')
 while a=='':
     a=input('nhập lại dữ liệu vào ')
-co=1
-while co==1:
-    for i in a:
-        if i!=',' and i!=' ' and not(i.isdigit()):
-            co=0
-            break
-    if co==1:
-        break
-    elif co==0:
-        a=input('nhập lại dữ liệu vào ')
-  
-    
-        # print (len(a))
-        # 昇順で並べる
-        # カンマで区切る
+
+while nhaplieu(a)==0:
+    a=input('nhập lại dữ liệu vào ')
+
+print(mylist)
